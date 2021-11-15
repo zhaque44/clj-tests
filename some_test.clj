ee4371ec31a7edffdc717eb2a083b364279bdeb7
Zubair Haque <haque.zubair@gmail.com>
2021-11-15 16:14:17 -0600

    Delete dupe

--- some_test.clj
+++
@@ -1,9 +0,0 @@
-(ns clj-tests.some-test
-  (:require [clojure.test :refer :all]
-    :use clojure.test
-        ring.mock.request
-        clj-tests.web))
-
-(deftest test-something-simple
-  (testing "A simple assertion"
-      (is (= 1 1))))

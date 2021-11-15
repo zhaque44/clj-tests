7130cf950890a82642305cf17844d5b83d902fd1
Zubair Haque <haque.zubair@gmail.com>
2021-11-15 16:10:43 -0600

    Delete src/test

--- src/test/clojure/com/climate/test/integration/simple_test.clj
+++
@@ -1,8 +0,0 @@
-(ns com.climate.test.integration.simple-test
-    (:require [clojure.test :refer :all]
-      [com.climate.configuration.env-config :as config]))
-
-
-(deftest ^:integration test-something-simple
-  (testing "A simple assertion"
-    (is (= 1 1))))

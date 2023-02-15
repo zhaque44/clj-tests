483b8eb14c41b0f4ab244a94469fe846947b9c16
zhaque44 <haque.zubair@gmail.com>
2023-02-15 13:38:44 -0600

    i

--- test/clj_tests/some_test.clj
+++ test/clj_tests/some_test.clj
@@ -1,8 +1,15 @@
 (ns clj-tests.some-test
-  (:require [clojure.test :refer :all]
-    :use clojure.test
-        ring.mock.request))
+ (:require [clojure.test :refer :all]
+           [cheshire.core :as json]
+           [clj-http.client :as http]
+           [environ.core :refer [env]]))
 
-(deftest test-something-simple
-  (testing "A simple assertion"
-      (is (= 1 1))))
+
+
+(deftest test-some
+  (testing "some test"
+    (let [req (http/get "http://site.api.espn.com/apis/site/v2/sports/football/nfl/teams")
+          response (json/parse-string (:body req) true)
+          _ (println response)]
+    (is (= 200 (:status req)))
+    (is (not (empty? (:sports response)))))))
